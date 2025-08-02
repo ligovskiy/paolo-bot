43134ef1ae41ad451ecfa793af9c11dc396b540e
ligovskiy <chipzidan@gmail.com>
2025-08-02 13:06:48 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-# Токен вашего Telegram бота (от BotFather)
-TELEGRAM_TOKEN = "8343972861:AAEXiWYV-7Agb-5yIlQBKD74QwhPUeI3n24"
+import os
 
-# ID Google таблицы
-GOOGLE_SHEET_ID = "176TIVG5c9zdBSqvI-Z528mHuLo2bHtxiZs7vMH_OLqM"
-
-# Имя листа в таблице
-SHEET_NAME = "Лист1"
-
-# OpenAI API ключ
-OPENAI_API_KEY = "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"
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+GOOGLE_SHEET_ID = os.getenv('GOOGLE_SHEET_ID')
+SHEET_NAME = os.getenv('SHEET_NAME', 'Лист1')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
